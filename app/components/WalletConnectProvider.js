1c914ec7cde77c6af5a6eae0a02f93e940de1ee7
iqbalfachry19 <ifachry@rocketmail.com>
2022-09-07 12:42:55 +0700

    hi

---
+++ app/components/WalletConnectProvider.js
@@ -0,0 +1,23 @@
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { ConnectionProvider,WalletProvider } from "@solana/wallet-adapter-react";
+import {WalletModalProvider} from "@solana/wallet-adapter-react-ui"
+import {GlowWalletAdapter,PhantomWalletAdapter,SlopeWalletAdapter} from "@solana/wallet-adapter-wallets"
+import { clusterApiUrl } from "@solana/web3.js";
+import {useMemo} from "react"
+export const WalletConnectProvider =({children})=>{
+const network = WalletAdapterNetwork.Devnet
+const endpoint = useMemo(()=>{
+    if(network === WalletAdapterNetwork.Devnet){
+        return "https://bitter-little-pond.solana-devnet.discover.quiknode.pro/49cda9e522c5b77061d67d900b2796e706b6c61f/"
+    }
+    return clusterApiUrl(network)
+},[network])
+const wallets = useMemo(()=>[new PhantomWalletAdapter()],[network])
+return (
+    <ConnectionProvider endpoint={endpoint}>
+<WalletProvider wallets={wallets} autoConnect>
+<WalletModalProvider>{children}</WalletModalProvider>
+</WalletProvider>
+    </ConnectionProvider>
+)
+}
